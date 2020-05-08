0adf01767ba3a10aa7a3b761a8aae508ad052e51
IMCP2000 <porwalchaitanya77@gmail.com>
2020-05-08 11:27:49 +0530

    :)

---
+++ tushi.sql
@@ -0,0 +1,23 @@
+create database school;
+use school;
+create table teacher(TNo int (56), TName varchar(67),TAddress varchar(100),Salary int(57),Dept_No int(78),Doj varchar(56));
+
+insert into teacher values(101,"P.K.Sasi","C.D.Colony",50000,201,"1997-08-09");
+
+insert into teacher values(102,"Raghuvanshi","C.D.Colony",50000,202,"1997-06-07");
+
+insert into teacher values(103,"Kiran Singh","Main Market",30000,203,"1993-06-05");
+
+insert into teacher values(104,"Bharati Reddy","B-Block",40000,204,"1997-04-03");
+
+insert into teacher values(105,"Rinku Banerjee","A-Block",40000,205,"1997-05-02");
+
+insert into teacher values(106,"Raj Kuwar","Grasim Colony",30000,206,"1994-05-04");
+
+insert into teacher values(107,"Veena Kundi","Chemical Colony",50000,207,"1995-03-07");
+
+insert into teacher values(108,"Shyam Kumar Shah","Jawahar Marg",50000,208,"1995-02-04");
+
+insert into teacher values(109,"Neeru Piyush","ABC Line",50000,209,"1995-03-07");
+
+insert into teacher values(1010,"Margeet Joseph","XYZ Colony",50000,210,"1993-06-04");
